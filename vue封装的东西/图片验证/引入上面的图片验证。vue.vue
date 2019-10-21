90a4d30690a4526fe752c26ce32d56106ca4bbc6
a1178985285 <1178985285@qq.com>
2019-10-21 18:02:57 +0800

    dsdds

---
+++ vue封装的东西/图片验证/引入上面的图片验证。vue.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div id="code">
+        <div>验证码 &nbsp;: &nbsp;&nbsp;&nbsp;</div>
+      <div class="code" @click="refreshCode">
+    <s-identify :identifyCode="identifyCode"></s-identify>
+  </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import SIdentify    from './o.vue'
+  export default {
+         data() {
+    return {
+      identifyCodes: "1234567890",
+      identifyCode: ""
+    };
+    
+  },
+  components:{
+SIdentify
+  },
+
+  mounted() {
+    this.identifyCode = "";
+    this.makeCode(this.identifyCodes, 4);
+  },
+
+    methods: {
+    randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    refreshCode() {
+      this.identifyCode = "";
+      this.makeCode(this.identifyCodes, 4);
+    },
+      makeCode(o, l) {
+      for (let i = 0; i < l; i++) {
+        this.identifyCode += this.identifyCodes[
+          this.randomNum(0, this.identifyCodes.length)
+        ];
+      }
+      console.log(this.identifyCode);
+    }
+    },
+
+
+
+  }
+</script>
+
+<style  scoped>
+#code{
+  display: flex;
+}
+.code {
+  width: 114px;
+  height: 40px;
+}
+</style>
