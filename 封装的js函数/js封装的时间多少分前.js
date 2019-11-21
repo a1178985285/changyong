ff83aff05d52a6189dd7963f2cd7f28f63c6385e
a1178985285 <1178985285@qq.com>
2019-11-21 18:05:43 +0800

    注释内容

--- 封装的js函数/js封装的时间多少分前.js
+++ 封装的js函数/js封装的时间多少分前.js
@@ -50,18 +50,18 @@
 
 // 单纯的时间戳转换
 					//时间戳转换
-		// function formatDate(date){
-		//         date = new Date(date);
-		//         var y=date.getFullYear();
-		//         var m=date.getMonth()+1;
-		//         var d=date.getDate();
-		//         var h=date.getHours();
-		//         var m1=date.getMinutes();
-		//         var s=date.getSeconds();
-		//         m = m<10?("0"+m):m;
-		// 		 m1 = m1<10?("0"+m1):m1;
-		//         d = d<10?("0"+d):d;
-		// 		s = s<10?("0"+s):s;
-		//         return y+"-"+m+"-"+d+" "+h+":"+m1+":"+s;
-		//     }
-		//   	console.log(formatDate(1570758848*1000));
+		function formatDate(date){
+		        date = new Date(date);
+		        var y=date.getFullYear();
+		        var m=date.getMonth()+1;
+		        var d=date.getDate();
+		        var h=date.getHours();
+		        var m1=date.getMinutes();
+		        var s=date.getSeconds();
+		        m = m<10?("0"+m):m;
+				 m1 = m1<10?("0"+m1):m1;
+		        d = d<10?("0"+d):d;
+				s = s<10?("0"+s):s;
+		        return y+"-"+m+"-"+d+" "+h+":"+m1+":"+s;
+		    }
+		  	console.log(formatDate(1570758848*1000));
